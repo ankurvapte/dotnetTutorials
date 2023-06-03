f9cd70eea6653b0729af3761e0cd581f862a8496
Ankur Apte <ankurapte@outlook.com>
2023-06-03 19:40:39 +0530

    Added Fourth Application to Git

---
+++ FourthApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine("What is your surname?");
+            var surname = Console.ReadLine();
+            Console.WriteLine($"{surname}");
+            
+            /* 
+             * Following style of string representation is called interpolated string 
+             */
+             
+            Console.WriteLine($"{Environment.NewLine}Hello, {name} {surname}, on {currentDate:d} at {currentDate:t}!");
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
